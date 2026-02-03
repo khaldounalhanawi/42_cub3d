08c7efe8306bd361cde94fc147df3dda5075fdc6
Khaldoun Alhanawi <khaldounalhanawi@users.noreply.github.com>
2026-02-03 13:11:54 +0100

    test maps -added

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kalhanaw <kalhanaw@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 17:29:54 by kalhanaw          #+#    #+#             */
-/*   Updated: 2026/01/19 13:43:45 by kalhanaw         ###   ########.fr       */
+/*   Updated: 2026/02/03 13:04:49 by kalhanaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,39 @@
 #include "parse.h"
 #include "validating.h"
 #include <stdio.h>
+
+static void	dump_init_data(t_init_data *data)
+{
+	int	i;
+
+	printf("\n========== INIT DATA DUMP ==========\n\n");
+	printf("Textures:\n");
+	printf("  North: %s\n", data->north_tex);
+	printf("  South: %s\n", data->south_tex);
+	printf("  West:  %s\n", data->west_tex);
+	printf("  East:  %s\n", data->east_tex);
+	printf("\nColors:\n");
+	printf("  Floor:   RGB(%d, %d, %d) [has_floor=%d]\n",
+		data->floor_color[0], data->floor_color[1], data->floor_color[2],
+		data->has_floor);
+	printf("  Ceiling: RGB(%d, %d, %d) [has_ceiling=%d]\n",
+		data->ceiling_color[0], data->ceiling_color[1], data->ceiling_color[2],
+		data->has_ceiling);
+	printf("\nMap:\n");
+	printf("  Width:  %d\n", data->map->width);
+	printf("  Height: %d\n", data->map->height);
+	printf("  Grid:\n");
+	i = 0;
+	while (i < data->map->height)
+	{
+		printf("    [%2d] %s\n", i, data->map->grid[i]);
+		i++;
+	}
+	printf("\nPlayer:\n");
+	printf("  Position: (%d, %d)\n", data->player_x, data->player_y);
+	printf("  Direction: %c\n", data->player_dir);
+	printf("\n====================================\n\n");
+}
 
 int main(int argc, char **argv)
 {
@@ -25,6 +58,7 @@ int main(int argc, char **argv)
 		exit_text ("Wrong count of arguments\n");
 	parse_input (&data, argv[1]);
 	validate_input (&data);
+	dump_init_data(&data);
 	// init_game (&game, data);
 	// set_hooks (&game);
 	// mlx_loop (game.mlx);
